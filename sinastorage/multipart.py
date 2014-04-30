270365a9d88ff1900229d98e3633c666f948eb60
poorevil <poorevil@gmail.com>
2014-04-30 11:51:22 +0800

    update

--- sinastorage/multipart.py
+++ sinastorage/multipart.py
@@ -2,21 +2,7 @@
 '''
 Created on 2014年4月29日
 
-@author: hanchao
 '''
-def part_lister(mpupload, part_number_marker=None):
-    """
-    A generator function for listing parts of a multipart upload.
-    """
-    more_results = True
-    part = None
-    while more_results:
-        parts = mpupload.get_all_parts(None, part_number_marker)
-        for part in parts:
-            yield part
-        part_number_marker = mpupload.next_part_number_marker
-        more_results = mpupload.is_truncated
-
 class Part(object):
     ''' 分片对象 '''
     def __init__(self, part_num, etag=None):
